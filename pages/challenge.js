992be90ad2b3bffa43741b973deefcbbe1149021
Kevin Chung <kchung@ctfd.io>
2025-08-16 17:01:03 -0400

    Add function to submit rating for challenge (#31)
    * Add function to submit rating against /api/v1/challenges/[challenge_id]/ratings

--- pages/challenge.js
+++ pages/challenge.js
@@ -163,3 +163,13 @@ export async function displaySolution(solutionId) {
     }
   }
 }
+
+export async function submitRating(challengeId, rating, review) {
+  const response = await CTFd.fetch(`/api/v1/challenges/${challengeId}/ratings`, {
+    method: "PUT",
+    body: JSON.stringify({ value: rating, review: review }),
+  });
+
+  const body = await response.json();
+  return body["data"];
+}
