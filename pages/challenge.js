f225e5e03c454eda59b9c4a4be93d3383eff717e
Kevin Chung <kchung@ctfd.io>
2025-10-29 00:05:59 -0400

    Fix solved behavior for checkSolution

--- pages/challenge.js
+++ pages/challenge.js
@@ -153,8 +153,9 @@ export function checkSolution(solutionState, challengeData, submissionStatus) {
   }
   if (solutionState == "hidden" || solutionState == "visible") {
     return false;
-  } else if (solutionState == "solved" && submissionStatus === "correct") {
-    return true;
+  } else if (solutionState == "solved") {
+    // If solutionState is solved we should check if the user got their submission right
+    return submissionStatus === "correct";
   } else {
     // We default to true in case there is a solution state that we are not aware of
     return true;
